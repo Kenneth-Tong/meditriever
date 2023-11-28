e9ddb065d09c09c2f1e9cc2be463e2816a59cc8b
Kenneth-Tong <ktong2233@gmail.com>
2023-11-27 21:26:56 -0500

    Flask Google Maps and FDA integration Version 1

--- src/app.py
+++ src/app.py
@@ -3,18 +3,30 @@ from dotenv import load_dotenv
 from flask import Flask
 
 from src.FDADrugAPI import get_drug
+from src.GoogleMapsAPI import get_id
 
 load_dotenv()
 
 FDA_API_KEY = os.getenv("FDA_API_KEY")  # Remember .env file!
-
+GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")  # Remember .env file!
 
 app = Flask(__name__)
 
 
 @app.route("/")
+def return_response():
+    json_list = list()
+    json_list.append(app_get_drug())
+    json_list.append(app_get_id())
+    return json_list
+
+
 def app_get_drug():
     return get_drug(FDA_API_KEY, "advil", 1)
+
+
+def app_get_id():
+    return get_id(GOOGLE_MAPS_API_KEY, "CVS")
 
 
 if __name__ == "__main__":
