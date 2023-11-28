6f62b86d5d64d5b36f97b857b541063c53089c49
Kenneth-Tong <ktong2233@gmail.com>
2023-11-27 20:34:59 -0500

    Testing Flask Version 1

--- src/FDADrugAPI.py
+++ src/FDADrugAPI.py
@@ -1,6 +1,11 @@
 import os
 import requests
 from dotenv import load_dotenv
+from flask import Flask
+
+from flask import Flask
+
+app = Flask(__name__)
 
 load_dotenv()
 
--- src/app.py
+++ src/app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-	return "Hello World"
+    return "Hello World"
+
 
 if __name__ == "__main__":
-	app.run(port=5000)
+    app.run(port=5000)
