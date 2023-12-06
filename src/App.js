c6abe7188d633b2d1af37bd62be4d6d493f865e5
apasingh <104095503+apasingh@users.noreply.github.com>
2023-12-06 02:30:10 -0500

    tried to add multiple pages, currently notrouting properly

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+
 import Home from './views/home.js';
+import AboutUs from './views/about-us'
+import Home from './views/home'
+import LoginPage from './views/login-page'
+import FAQs from './views/fa-qs'
+import NotFound from './views/not-found'
 
 function App() {
   return (
-    <Home />
+    <Routes>
+      <Route component={Home} exact path ="/home"
+      <Route component={AboutUs} exact path="/about-us" />
+      <Route component={Home} exact path="/" />
+      <Route component={LoginPage} exact path="/login-page" />
+      <Route component={FAQs} exact path="/fa-qs" />
+      <Route component={NotFound} path="**" />
+      <Redirect to="**" />
+    </Routes>
   );
 }
 
