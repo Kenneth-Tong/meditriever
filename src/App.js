4ee44773e757079c18eb6c51eb8fb959249bb7dd
apasingh <104095503+apasingh@users.noreply.github.com>
2023-12-06 02:54:52 -0500

    added login page

--- src/App.js
+++ src/App.js
@@ -9,13 +9,7 @@ import NotFound from './views/not-found.js'
 
 function App() {
   return (
-    <Routes>
-      <Route component={Home} exact path ="/home" /> 
-      <Route component={AboutUs} exact path="/about-us" />
-      <Route component={Home} exact path="/" />
-      <Route component={FAQs} exact path="/fa-qs" />
-      <Route component={NotFound} path="**" />
-    </Routes>
+    <Home />
   );
 }
 
