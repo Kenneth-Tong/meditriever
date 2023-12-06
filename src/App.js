4dadfcf155e8eeb61623d54b0b9e11ca03aa126c
apasingh <104095503+apasingh@users.noreply.github.com>
2023-12-06 03:04:06 -0500

    IT RUNSgit status got the routing to work

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import { Route, Routes } from 'react-router-dom';
 
 import Home from './views/home.js';
 import AboutUs from './views/about-us.js'
-//import LoginPage from './views/login-page.js'
+import LoginPage from './views/login-page.js'
 import FAQs from './views/fa-qs.js'
 import NotFound from './views/not-found.js'
 
 function App() {
   return (
-    <Home />
+    <div>
+      <nav>...</nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/AboutUs" element={<AboutUs />} /> 
+          <Route path="/login-page" element={<LoginPage />} />
+          <Route path="/faqs" element={<FAQs />} />
+          <Route path="/404" element={<NotFound />} />
+      </Routes>
+  </div>
   );
 }
 
