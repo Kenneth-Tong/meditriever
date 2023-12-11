ca093741ede863d1d06c19219b5f9212c73b8e40
KluelessKat <kathyzyx@bu.edu>
2023-12-11 11:40:07 -0500

    Added SQLAlchemy + prototype Data Table

--- src/app.py
+++ src/app.py
@@ -2,7 +2,21 @@ import os
 import requests
 from dotenv import load_dotenv
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+#User class that maps to data table in db
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    date = db.Column(db.Integer, unique=True, nullabele=False)
+
+#Initialize SQLAlchemy extension and configure database url
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///meditriever/src/UserData.db' #not sure if this is the correct path
+db = SQLAlchemy(app)
 
 from src.FDADrugAPI import get_drug
 from src.GoogleMapsAPI import get_id
@@ -12,8 +26,7 @@ load_dotenv()
 FDA_API_KEY = os.getenv("FDA_API_KEY")  # Remember .env file!
 GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")  # Remember .env file!
 
-app = Flask(__name__)
-
+#app = Flask(__name__)
 
 @app.route("/")
 def return_response():
@@ -33,3 +46,5 @@ def app_get_id(location):
 
 if __name__ == "__main__":
     app.run(port=5000)
+    db.create_all()
+    app.run(debug=True)
