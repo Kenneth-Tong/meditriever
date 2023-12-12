3f8d3917bfc9d6d5ab47730228380da80f6fa8d0
KluelessKat <kathyzyx@bu.edu>
2023-12-11 22:23:01 -0500

    Oauth Second Try

--- src/views/login-page.js
+++ src/views/login-page.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Login from './login'; // Import the Login component
+import Login from '../components/login'; // Import the Login component
 
 import Script from 'react-dangerous-html'
 import { Helmet } from 'react-helmet'
